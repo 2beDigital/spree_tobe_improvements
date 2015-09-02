d9435f40ce073a3ffb8bb07aa896d6c2612ba366
2bedigital <hector@2bedigital.com>
2015-09-02 23:32:14 +0200

    monkey patching, ransack include problems

---
+++ app/models/spree_i18n/translatable.rb
@@ -0,0 +1,13 @@
+module SpreeI18n
+  module Translatable
+    extend ActiveSupport::Concern
+    include Spree::RansackableAttributes
+
+    included do |klass|
+      accepts_nested_attributes_for :translations
+      klass.whitelisted_ransackable_associations |= ['translations']
+    end
+
+  end
+end
+
