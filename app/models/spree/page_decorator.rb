f1a81fcdf0e9afd0c0296a2be569ffb769a4334a
picazoH <hector@ahaaa.es>
2015-09-04 17:09:44 +0200

    RansackableAttributes problems with includes and concern part2

--- app/models/spree/page_decorator.rb
+++ app/models/spree/page_decorator.rb
@@ -3,7 +3,8 @@ module Spree
     translates :title, :body, :slug, :meta_description,
                :meta_keywords, :meta_title, :foreign_link,
                :fallbacks_for_empty_translations => true
-    #include Spree::RansackableAttributes
+    # Classpath bug; undefined method `whitelisted_ransackable_associations'
+    include Spree::RansackableAttributes
     include SpreeI18n::Translatable
   end
 end
