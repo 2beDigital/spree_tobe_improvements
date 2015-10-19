7ef3d96675bb396cae178e63b0bf30adf71bcc6a
Noel <noel@2bedigital.com>
2015-10-19 08:12:46 +0100

    change flash error to notice

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::CheckoutController.class_eval do
   def ensure_checkout_allowed
     check = @order.checkout_allowed?
     unless check == true
-      redirect_to spree.cart_path, :flash => {:error => I18n.t("checkout_allowed_errors.#{check}") + Spree::Config[:minimum_order_value] + " " + Spree::Config[:currency]}
+      redirect_to spree.cart_path, :flash => {:notice => I18n.t("checkout_allowed_errors.#{check}") + Spree::Config[:minimum_order_value] + " " + Spree::Config[:currency]}
     end
   end
 end
