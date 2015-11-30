79cc915b8412123d80e3c913aa932c00ae24cf30
Noel <noel@2bedigital.com>
2015-11-30 13:31:00 +0000

    fixes on trackers

--- app/overrides/spree/google_tag_manager/add_field_for_gtm_on_admin.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/admin/trackers/_form',
-                     :name => 'add_fields_fot_gtm_on_admin',
-                     :insert_bottom => 'div[data-hook="admin_tracker_form_fields"]',
-                     :partial => 'spree/admin/trackers/google_tag_manager_for_admin')
--- app/overrides/spree/google_tag_manager/add_gtm_id_to_table_at_trackers_index.rb
+++ app/overrides/spree/google_tag_manager/add_gtm_id_to_table_at_trackers_index.rb
@@ -5,7 +5,13 @@ Deface::Override.new(:virtual_path => 'spree/admin/trackers/index',
                      :closing_selector => 'erb[silent]:contains("end")')
 
 Deface::Override.new(:virtual_path => 'spree/admin/trackers/index',
-                     :name => 'add_gtm_id_to_table',
+                     :name => 'replace_table',
                      :sequence => 2,
+                     :replace => 'erb[loud]:contains("Spree.t(:google_analytics_id)")',
+                     :text => '<%= Spree.t(:analytics_trackers) %>')
+
+Deface::Override.new(:virtual_path => 'spree/admin/trackers/index',
+                     :name => 'add_gtm_id_to_table',
+                     :sequence => 3,
                      :insert_top => 'tbody',
                      :partial => 'spree/admin/trackers/new_table' )
