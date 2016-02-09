39c73c3977bd6ac932d86ab11e32ca7df6ea5a50
SergiCarrasco <sergi@2bedigital.com>
2016-02-09 23:43:05 +0100

    Bug in export users to csv. Exported only first page (Bug Fix/BF-17).

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -1,5 +1,12 @@
 module Spree::Admin
   UsersController.class_eval do
-    respond_to :html, :csv
+    alias_method :index_original, :index unless method_defined? :index_original
+    respond_to :csv, :only => [:index]
+    def index
+      respond_with(@collection) do |format|
+        format.csv { @collection = @search.result and return }
+        format.any { index_original }
+      end
+    end
   end
  end
