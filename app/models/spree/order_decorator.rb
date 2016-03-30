4874609040f70e970365cc9517c167342816fc3d
SergiCarrasco <sergi@2bedigital.com>
2016-03-30 02:17:00 +0200

    Added deliver_order_confirmation_mail to Order decorator

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,6 +5,17 @@ Spree::Order.class_eval do
     true
   end
 
+	# override this to also set current user locale for email i18n
+	def deliver_order_confirmation_email
+		update_attribute(:locale, I18n.locale)
+		begin
+			OrderMailer.confirm_email(self.id).deliver
+		rescue Exception => e
+			logger.error("#{e.class.name}: #{e.message}")
+			logger.error(e.backtrace * "\n")
+		end
+	end
+	
   private
 
   def after_cancel
