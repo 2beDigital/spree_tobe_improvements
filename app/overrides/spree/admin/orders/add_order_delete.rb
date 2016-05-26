45e8b61aafeac142eb02419fb853f9e0a80b718a
Noel <noel@2bedigital.com>
2016-05-26 06:52:31 +0100

    change translate on confirm message

--- app/overrides/spree/admin/orders/add_order_delete.rb
+++ app/overrides/spree/admin/orders/add_order_delete.rb
@@ -3,5 +3,5 @@ Deface::Override.new(:virtual_path => "spree/admin/orders/index",
                      :insert_bottom => "[data-hook='admin_orders_index_row_actions']",
                      :text =>  "<% css_class = (order.complete?) ? 'fa fa-trash icon_link with-tip no-text' : 'delete-resource fa fa-trash icon_link with-tip no-text' %>
                      		   	<% if !order.complete? %>
-                               		<%= link_to '', admin_order_path(order.id), class: css_class, method: :delete, data: {confirm: t(:confirm)} %>
+                               		<%= link_to '', admin_order_path(order.id), class: css_class, method: :delete, data: {confirm: Spree.t(:are_you_sure_you_want_to_delete_this_record)} %>
                                	<% end %> ")
