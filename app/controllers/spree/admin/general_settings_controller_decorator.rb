a15720f7d5a0599ba2f632eea364e9ff75b6ce84
Noel <noel@2bedigital.com>
2016-06-29 07:41:00 +0100

    Improvements on general settings

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++ app/controllers/spree/admin/general_settings_controller_decorator.rb
@@ -2,8 +2,6 @@ Spree::Admin::GeneralSettingsController.class_eval do
   alias_method :edit_original, :edit unless method_defined? :edit_original
   def edit
     edit_original
-    @preferences_currency << :minimum_order_value
-    @preferences_currency << :backorder_charge
-    @preferences_currency << :check_categories
+    @preferences_order_and_shipments = [:minimum_order_value, :backorder_charge, :check_categories, :show_sku]
   end
 end
---
+++ app/overrides/spree/admin/general_settings/edit/add_preferences_orders_and_shipments.rb
@@ -0,0 +1,14 @@
+Deface::Override.new(virtual_path: 'spree/admin/general_settings/edit',
+					 name: 'add_preferences_orders_and_shipments',
+					 insert_top: "div#preferences div.row div.alpha.six.columns",
+				     text: '<fieldset class="currency no-border-bottom">
+						    	<legend align="center"><%= t(:orders_and_shipments_settings)%></legend>
+						    	<% @preferences_order_and_shipments.each do |key|
+						        	type = Spree::Config.preference_type(key) %>
+						        	<div class="field">
+						          	<%= label_tag(key, Spree.t(key)) + tag(:br) if type != :boolean %>
+						          	<%= preference_field_tag(key, Spree::Config[key], :type => type) %>
+						          	<%= label_tag(key, Spree.t(key)) + tag(:br) if type == :boolean %>
+						        	</div>
+						    	<% end %>
+						  	</fieldset>')
