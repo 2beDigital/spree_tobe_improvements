1ec2fa0fa7f7c564de41eacfdb9c407630fa97b7
Noel <noel@2bedigital.com>
2016-06-29 07:44:53 +0100

    bug on override

--- app/overrides/spree/admin/general_settings/edit/add_preferences_orders_and_shipments.rb
+++ app/overrides/spree/admin/general_settings/edit/add_preferences_orders_and_shipments.rb
@@ -2,7 +2,7 @@ Deface::Override.new(virtual_path: 'spree/admin/general_settings/edit',
 					 name: 'add_preferences_orders_and_shipments',
 					 insert_top: "div#preferences div.row div.alpha.six.columns",
 				     text: '<fieldset class="currency no-border-bottom">
-						    	<legend align="center"><%= t(:orders_and_shipments_settings)%></legend>
+						    	<legend align="center"><%= Spree.t(:orders_and_shipments_settings)%></legend>
 						    	<% @preferences_order_and_shipments.each do |key|
 						        	type = Spree::Config.preference_type(key) %>
 						        	<div class="field">
