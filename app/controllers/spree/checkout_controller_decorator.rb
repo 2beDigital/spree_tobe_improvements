ea7684bf9bdf8a18a0947f26c06c1263ffb4d9ba
Noel <noel@2bedigital.com>
2016-10-24 15:40:54 +0100

    bug on checkout with user not logged

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -34,7 +34,7 @@ Spree::CheckoutController.class_eval do
      def before_address
         # if the user has a default address, a callback takes care of setting
         # that; but if he doesn't, we need to build an empty one here
-        @order.bill_address = spree_current_user.bill_address || Address.build_default
-        @order.ship_address = spree_current_user.ship_address || Address.build_default if @order.checkout_steps.include?('delivery')
+        @order.bill_address = !spree_current_user.blank? ? spree_current_user.bill_address : Address.build_default
+        @order.ship_address = ( !spree_current_user.blank? ? spree_current_user.ship_address : Address.build_default ) if @order.checkout_steps.include?('delivery')
     end
 end
