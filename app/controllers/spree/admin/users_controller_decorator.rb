b58394e807612671ddc1225e7338fbca3d9dcd89
root <sergi@2bedigital.com>
2017-01-30 13:04:15 +0100

    v2.4.41: BF-254. Users index sorted by createad_at desc by default

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -1,12 +1,21 @@
 module Spree::Admin
   UsersController.class_eval do
+	  #sort by created_at desc by default.
+		before_action :sort, only: :index
     alias_method :index_original, :index unless method_defined? :index_original
     respond_to :csv, :only => [:index]
     def index
       respond_with(@collection) do |format|
         format.csv { @collection = @search.result and return }
-        format.any { index_original }
+        format.any { index_original and return }
       end
     end
+		
+		def sort
+			if (!params.has_key?(:q))
+				redirect_to(admin_users_url('q[s]'=>'created_at desc')) and return
+			end		
+		end
+		
   end
  end
