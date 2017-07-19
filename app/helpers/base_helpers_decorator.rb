9aa2d0b84e7af09ae8a6925d43bcb1f8970b6ad6
Noel <noel@2bedigital.com>
2017-07-19 09:45:12 +0100

    bug with google analitycs

--- app/helpers/base_helpers_decorator.rb
+++ app/helpers/base_helpers_decorator.rb
@@ -6,6 +6,10 @@ def get_facebook_pixel_id
   Spree::Tracker.where('facebook_pixel_id <> ? AND active = ?', '', true).last
 end
 
+def get_google_analitics_tracker
+  Spree::Tracker.where('analytics_id <> ? AND active = ?', '', true).last
+end
+
 def backorder_charge
   return Spree::Config[:backorder_charge]
 end
